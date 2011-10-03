278619a149e69e30077d3d0122f248de1acb6254
Darrick Wiebe <darrick@innatesoftware.com>
2011-10-03 16:58:31 -0400

    Created repo

---
+++ lib/pacer-neo4j-algo.rb
@@ -0,0 +1,9 @@
+require "pacer-neo4j-algo/version"
+
+module Pacer
+  module Neo4j
+    module Algo
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pacer-neo4j-algo/version.rb
@@ -0,0 +1,7 @@
+module Pacer
+  module Neo4j
+    module Algo
+      VERSION = "0.0.1"
+    end
+  end
+end
